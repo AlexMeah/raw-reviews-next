fc1231e40aef577ee3886438e848cddae74a13dc
Alex Meah <alexmeahdotcom@gmail.com>
2017-05-09 20:40:03 +0100

    Add t&cs

--- layouts/Basic.js
+++ layouts/Basic.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Container = styled.div`
     padding: 3rem;
@@ -11,6 +12,7 @@ const Layout = ({ children, ...rest }) => (
     <Container {...rest}>
         <Header />
         {children}
+        <Footer />
     </Container>
 );
 
