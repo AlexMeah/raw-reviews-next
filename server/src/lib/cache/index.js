475b83ad33341c5a7fdc80d5be8ece96ea3cd585
Alex Meah <alexmeahdotcom@gmail.com>
2017-05-12 18:35:51 +0100

    Wait for all keys to be deleted

--- server/src/lib/cache/index.js
+++ server/src/lib/cache/index.js
@@ -31,7 +31,7 @@ module.exports = {
     },
     delPattern(pattern) {
         if (client) {
-            return client.keysAsync(pattern).then(keys => keys.map(key => client.delAsync(key)));
+            return client.keysAsync(pattern).then(keys => Promise.all(keys.map(key => client.delAsync(key))));
         }
 
         return Promise.resolve();
