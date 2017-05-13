a85ffb202d058faadee4b342677e73876a7353cd
Alex Meah <alexmeahdotcom@gmail.com>
2017-05-13 11:33:16 +0100

    Remove ad test

--- components/Ad/index.js
+++ components/Ad/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 // 3129031971
 
 export default ({ slot }) => (
-    <div>
+    <div className="row center-xs">
         <Helmet
             script={[
                 {
@@ -14,12 +14,11 @@ export default ({ slot }) => (
             ]}
         />
         <ins
-            className="adsbygoogle"
+            className="adsbygoogle col-xs-12"
             style={{ display: 'block' }}
             data-ad-client="ca-pub-8155196363770236"
             data-ad-slot={slot}
             data-ad-format="auto"
-            data-ad-test="on"
         />
         <script
             dangerouslySetInnerHTML={{
