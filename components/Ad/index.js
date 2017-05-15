ac19b11d2dc65b4636329ad9f5be8e432fad8c27
Alex Meah <alexmeahdotcom@gmail.com>
2017-05-15 21:18:27 +0100

    Hide ads in dev

--- components/Ad/index.js
+++ components/Ad/index.js
@@ -1,29 +1,33 @@
 import Helmet from 'react-helmet';
 import React from 'react';
 
-// 3129031971
+export default ({ slot }) => {
+    if (process.env.NODE_ENV === 'dev') {
+        return null;
+    }
 
-export default ({ slot }) => (
-    <div className="row center-xs">
-        <Helmet
-            script={[
-                {
-                    src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-                    async: true
-                }
-            ]}
-        />
-        <ins
-            className="adsbygoogle col-xs-12"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-8155196363770236"
-            data-ad-slot={slot}
-            data-ad-format="auto"
-        />
-        <script
-            dangerouslySetInnerHTML={{
-                __html: '(adsbygoogle = window.adsbygoogle || []).push({});'
-            }}
-        />
-    </div>
-);
+    return (
+        <div className="row center-xs">
+            <Helmet
+                script={[
+                    {
+                        src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+                        async: true
+                    }
+                ]}
+            />
+            <ins
+                className="adsbygoogle col-xs-12"
+                style={{ display: 'block' }}
+                data-ad-client="ca-pub-8155196363770236"
+                data-ad-slot={slot}
+                data-ad-format="auto"
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: '(adsbygoogle = window.adsbygoogle || []).push({});'
+                }}
+            />
+        </div>
+    );
+};
