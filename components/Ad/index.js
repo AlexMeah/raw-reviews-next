4a5e7f4c3c275800a546de5aa82ca20bc55097d2
Alex Meah <alexmeahdotcom@gmail.com>
2017-05-22 16:26:37 +0100

    Potential Adsense fix

--- components/Ad/index.js
+++ components/Ad/index.js
@@ -1,35 +1,43 @@
 import Helmet from 'react-helmet';
 import React from 'react';
 
-export default ({ slot }) => {
-    if (process.env.NODE_ENV === 'dev') {
-        return null;
+class Ad extends React.Component {
+
+    componentDidMount() {
+        if (window)  {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        }
     }
 
-    return (
-        <div className="row center-xs">
-            <Helmet
-                script={[
-                    {
-                        src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-                        async: true
-                    }
-                ]}
-            />
-            <div className="col-xs-12">
-                <ins
-                    className="adsbygoogle"
-                    style={{ display: 'block', marginBottom: '2rem' }}
-                    data-ad-client="ca-pub-8155196363770236"
-                    data-ad-slot={slot}
-                    data-ad-format="auto"
+    render() {
+        const { slot } = this.props;
+
+        if (process.env.NODE_ENV === 'dev') {
+            return null;
+        }
+
+        return (
+            <div className="row center-xs">
+                <Helmet
+                    script={[
+                        {
+                            src: '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+                            async: true
+                        }
+                    ]}
                 />
+                <div className="col-xs-12">
+                    <ins
+                        className="adsbygoogle"
+                        style={{ display: 'block', marginBottom: '2rem' }}
+                        data-ad-client="ca-pub-8155196363770236"
+                        data-ad-slot={slot}
+                        data-ad-format="auto"
+                    />
+                </div>
             </div>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: '(adsbygoogle = window.adsbygoogle || []).push({});'
-                }}
-            />
-        </div>
-    );
-};
+        );
+    }
+}
+
+export default Ad;
