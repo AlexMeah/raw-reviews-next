4c52f562ed008959b4299e4c6c23a45ded849a53
Alex Meah <alexmeahdotcom@gmail.com>
2017-05-25 20:42:58 +0100

    Add outline buttons

--- components/Button/buttonStyles.js
+++ components/Button/buttonStyles.js
@@ -32,54 +32,69 @@ const buttonStyles = css`
     font-weight: 400;
     line-height: 1;
     padding: ${props => `${(sizes[props.size] || 1) * 1.2}rem ${(sizes[props.size] || 1) * 1.6}rem`};
-    font-size: ${props => `${(sizes[props.size] || 1) * 1}em`}    ;
+    font-size: 1em;
     border-radius: ${styleVars.radius};
     font-family: inherit;
-    color: ${props => styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};
-    background-color: ${props => styleVars.colors[props.color] || styleVars.colors.primary};
+    background: transparent;
+    ${props => !props.outline && `color: ${styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};`}
+    ${props => props.outline && `color: ${styleVars.colors[`${props.color}`] || styleVars.colors.primary};`}
+    ${props => !props.outline && `background-color: ${styleVars.colors[props.color] || styleVars.colors.primary};`}
+    ${props => props.outline && `border: 2px solid ${styleVars.colors[props.color] || styleVars.colors.primary};`}
     margin-bottom: 2rem;
     ${props => props.block && 'width: 100%;'}
+    ${props => props.align && `vertical-align: ${props.align};`}
 
     &:last-child {
         margin-bottom: 0;
     }
 
     &:hover {
-        color: ${props => styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};
-        background-color: ${props => dark(styleVars.colors[props.color] || styleVars.colors.primary)};
+        ${props => !props.outline && `color: ${styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};`}
+        ${props => props.outline && `color: ${styleVars.colors[`${props.color}`] || styleVars.colors.primary};`}
+        ${props => !props.outline && `background-color: ${dark(styleVars.colors[props.color] || styleVars.colors.primary)};`}
+        ${props => props.outline && `border: 2px solid ${dark(styleVars.colors[props.color] || styleVars.colors.primary)};`}
     }
 
     &:focus {
-        color: ${props => styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};
-        background-color: ${props => dark(styleVars.colors[props.color] || styleVars.colors.primary)};
+        ${props => !props.outline && `color: ${styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};`}
+        ${props => props.outline && `color: ${styleVars.colors[`${props.color}`] || styleVars.colors.primary};`}
+        ${props => !props.outline && `background-color: ${dark(styleVars.colors[props.color] || styleVars.colors.primary)};`}
+        ${props => props.outline && `border: 2px solid ${dark(styleVars.colors[props.color] || styleVars.colors.primary)};`}
     }
 
     &:active {
-        color: ${props => styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};
-        background-color: ${props => dark(styleVars.colors[props.color] || styleVars.colors.primary)};
+        ${props => !props.outline && `color: ${styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};`}
+        ${props => props.outline && `color: ${styleVars.colors[`${props.color}`] || styleVars.colors.primary};`}
+        ${props => !props.outline && `background-color: ${dark(styleVars.colors[props.color] || styleVars.colors.primary)};`}
+        ${props => props.outline && `border: 2px solid ${dark(styleVars.colors[props.color] || styleVars.colors.primary)};`}
         background-image: none;
         outline: 0;
 
         &:focus,
         &:hover {
-            color: ${props => styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};
-            background-color: ${props => darker(styleVars.colors[props.color] || styleVars.colors.primary)};
+            ${props => !props.outline && `color: ${styleVars.colors[`${props.color}Color`] || styleVars.colors.primaryColor};`}
+            ${props => props.outline && `color: ${styleVars.colors[`${props.color}`] || styleVars.colors.primary};`}
+            ${props => !props.outline && `background-color: ${darker(styleVars.colors[props.color] || styleVars.colors.primary)};`}
+            ${props => props.outline && `border: 2px solid ${darker(styleVars.colors[props.color] || styleVars.colors.primary)};`}
         }
     }
 
     &:disabled {
         cursor: not-allowed;
         opacity: .2;
-        background-color: ${styleVars.colors.muted};
+        ${props => !props.outline && `background-color: ${styleVars.colors.muted};`}
+        ${props => props.outline && `border: 2px solid ${styleVars.colors.muted};`}
         color: ${styleVars.colors.mutedColor};
 
         &.focus,
         &:focus {
-            background-color: ${styleVars.colors.muted};
+            ${props => !props.outline && `background-color: ${styleVars.colors.muted};`}
+            ${props => props.outline && `border: 2px solid ${styleVars.colors.muted};`}
         }
 
         &:hover {
-            background-color: ${styleVars.colors.muted};
+            ${props => !props.outline && `background-color: ${styleVars.colors.muted};`}
+            ${props => props.outline && `border: 2px solid ${styleVars.colors.muted};`}
         }
     }
 `;
